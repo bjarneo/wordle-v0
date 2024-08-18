20728524d4924b08b78474c617d00696a7417ea9
bjarneo <bjarne.oeverli@gmail.com>
2024-08-18 10:25:42 +0200

    feat: add shadcn and next

---
+++ README.md
@@ -0,0 +1,8 @@
+Created by https://v0.dev/chat
+
+## Run it
+```bash
+npm install
+
+npm run dev
+```
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Wordle from "@/components/ui/wordle";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Wordle />
+    </div>
+  );
+}
